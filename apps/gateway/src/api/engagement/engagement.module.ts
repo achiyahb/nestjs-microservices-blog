cfd3daa60175c3b36945b92d4832bcd8d05912e5
achiyahaviv <ahiya@coing.co>
2022-03-09 09:20:36 +0200

    remove deleted service file import

--- apps/gateway/src/api/engagement/engagement.module.ts
+++ apps/gateway/src/api/engagement/engagement.module.ts
@@ -1,9 +1,7 @@
 import { Module } from '@nestjs/common';
-import { EngagementService } from './engagement.service';
 import { EngagementController } from './engagement.controller';
 
 @Module({
   controllers: [EngagementController],
-  providers: [EngagementService],
 })
 export class EngagementModule {}
