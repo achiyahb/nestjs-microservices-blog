f48a563e54a557b573a078534ad7bcb7b2f19aa1
achiyahaviv <ahiya@coing.co>
2022-03-09 08:51:27 +0200

    set blogs app as tcp microservice

---
+++ apps/blogs/src/blogs-app.module.ts
@@ -0,0 +1,26 @@
+import { Module } from '@nestjs/common';
+import { MongooseModule } from '@nestjs/mongoose';
+import { ConfigModule, ConfigService } from '@nestjs/config';
+import { blogsConfig } from '@config/configs/services-config/blogs-config';
+import { PostsModule } from './posts/posts.module';
+import { BlogsModule } from './blogs/blogs.module';
+import { mongodbConfig } from '@config/configs/mongodb-config';
+
+@Module({
+  imports: [
+    MongooseModule.forRootAsync({
+      imports: [ConfigModule],
+      useFactory: async (config: ConfigService) => ({
+        uri: config.get('mongodb.uri'),
+      }),
+      inject: [ConfigService],
+    }),
+    ConfigModule.forRoot({
+      isGlobal: true,
+      load: [blogsConfig, mongodbConfig],
+    }),
+    PostsModule,
+    BlogsModule,
+  ],
+})
+export class BlogsAppModule {}
--- apps/blogs/src/main.ts
+++ apps/blogs/src/main.ts
@@ -1,8 +1,24 @@
 import { NestFactory } from '@nestjs/core';
-import { BlogsModule } from './blogs.module';
+import { ConfigService } from '@nestjs/config';
+import {
+  TcpOptions,
+  Transport,
+  MicroserviceOptions,
+} from '@nestjs/microservices';
+import { BlogsAppModule } from './blogs-app.module';
 
 async function bootstrap() {
-  const app = await NestFactory.create(BlogsModule);
-  await app.listen(3000);
+  const app = await NestFactory.createApplicationContext(BlogsAppModule);
+  const configService = app.get(ConfigService);
+  const microservice =
+    await NestFactory.createMicroservice<MicroserviceOptions>(BlogsAppModule, {
+      transport: Transport.TCP,
+      options: {
+        host: configService.get('blogs.host'),
+        port: configService.get('blogs.port'),
+      },
+    } as TcpOptions);
+  await microservice.listen();
 }
+
 bootstrap();
