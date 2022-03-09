6370f7e4e4f29f0778f0aefa5e3ea838fdfb3fe5
achiyahaviv <ahiya@coing.co>
2022-03-09 08:47:35 +0200

    posts module - microservice side

---
+++ apps/blogs/src/posts/interfaces/post.interface.ts
@@ -0,0 +1,11 @@
+export interface PostInterface {
+  id: string;
+  title: string;
+  description: string;
+  content: string;
+  image: string;
+  link: string;
+  isPublished: boolean;
+  createdAt: string;
+  updatedAt: string;
+}
